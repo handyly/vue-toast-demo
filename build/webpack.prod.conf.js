527182c59a0bea6c9348fbea0276472d26c12a47
yanleying <leying.yan@hand-china.com>
2019-09-01 17:57:48 +0800

    'update'

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -32,8 +32,8 @@ const webpackConfig = merge(baseWebpackConfig, {
   output: {
     path: path.resolve(__dirname, '../dist'),
     publicPath: '/dist/',
-    filename: 'vue-toast-demo.js',
-    library: 'vueToastDemo', // library指定的就是你使用require时的模块名，这里便是require("vue-toast-demo")
+    filename: 'vue-toast-yly.js',
+    library: 'vueToastYly', // library指定的就是你使用require时的模块名，这里便是require("vue-toast-yly")
     libraryTarget: 'umd', //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
     umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
   },
